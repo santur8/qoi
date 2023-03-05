ba5738857d525ff77845242e17d8d9cd4a7204d6
santur8 <tgrogers1221@gmail.com>
2023-03-05 18:35:46 -0500

    Move file_exists to main.rs

--- src/encode.rs
+++ src/encode.rs
@@ -1,14 +1,6 @@
-use std::path::Path;
+use std::io::*;
+use std::fs::File;
 
-const IMAGE_PATH: &str = "images/";
-
-pub fn file_exists(path: &str) -> bool {
-	let full_path = format!("{}{}", IMAGE_PATH, path);
-	let p = Path::new(&full_path);
-	let exists: bool = p.exists();
-	if !exists {
-		print!("The file at path {} does not exist. ", full_path);
-		println!("Verify that the file exists in the images directory.");
-	}
-	exists
+pub fn encode(png: Result<File>) {
+	// encode the file specified by path
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
 use std::env;
+use std::io::*;
+use std::fs::File;
+use std::path::Path;
 
 mod encode;
 mod decode;
+
+const IMAGE_PATH: &str = "images/";
 
 fn main() {
 	println!("");
 	// verify that we have 3 arguments: executable, operation, and file
 	let argc: usize = env::args().len();
 	let argv: Vec<String> = env::args().collect();
-	if argc != 3 || !encode::file_exists(&argv[2]) {
+    let full_path: String = format!("{}{}", IMAGE_PATH, &argv[2]);
+	if argc != 3 || !file_exists(&full_path) {
 		println!("Invalid arguments supplied.\nArguments should follow the form \"cargo run <file operation> <file>\"");
 		std::process::exit(-1);
 	}
 
+	let mut file: Result<File> = File::create(full_path);
 	if &argv[1] == "encode" {
 		println!("encoding");
+		encode::encode(file);
 	} else if &argv[1] == "decode" {
 		println!("decoding");
 	} else {
 		println!("Invalid file operation. Choose from the following:\nencode\ndecode");
+		std::process::exit(-1);
 	}
+}
+
+/* Verify that the image to encode exists */ 
+fn file_exists(path: &str) -> bool {
+    let p = Path::new(&path);
+    let exists: bool = p.exists();
+    if !exists {
+        print!("The file at path {} does not exist. ", path);
+        println!("Verify that the file exists in the images directory.");
+    }
+    exists
 }
